7fd4f27e6191fa7be139cdb3efbb7e2eb0dbfdb7
Joseph Dillon <dillonjl@ornl.gov>
2023-03-30 10:49:32 -0400

    improved observable streams

--- src/app/components/poll-form/poll-form.component.ts
+++ src/app/components/poll-form/poll-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Observable} from "rxjs";
+import { map, Observable } from 'rxjs';
 import {PollOption} from "src/app/models/poll-option"
 import {PollService} from "src/app/services/poll.service";
 import {Poll} from "../../models/poll";
@@ -10,22 +10,19 @@ import {Poll} from "../../models/poll";
   styleUrls: ['./poll-form.component.css']
 })
 export class PollFormComponent implements OnInit{
-  polls: Observable<Poll[]> = new Observable<Poll[]>();
+  pollQuestion$: Observable<string> = new Observable<string>();
+  pollOptions$: Observable<PollOption[]> = new Observable<PollOption[]>();
   selectedOption: PollOption;
-
-  testOptions: PollOption[] = [
-    {id: "1", text: "option1", voteCount: 5},
-    {id: "2", text: "option2", voteCount: 10},
-    {id: "3", text: "option3", voteCount: 15},
-    {id: "4", text: "option4", voteCount: 20},
-  ]
 
   constructor(private pollService: PollService) {
     this.selectedOption = {id: "no id",text: "not selected", voteCount: 0};
   }
 
   ngOnInit() {
-    this.polls = this.pollService.getPolls();
+    this.pollOptions$ = this.pollService.getPolls().pipe(map(poll => {
+      return poll?.options
+    }));
+    this.pollQuestion$ = this.pollService.getPolls().pipe(map(poll => poll?.question));
   }
 
 }
